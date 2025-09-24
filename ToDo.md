fb5f2cc7da4c054e5c2543b60a70f6742b99cbc3
Eduardo_S <segura2011@live.com>
2025-09-24 01:30:19 -0600

    Inicio de proyecto

---
+++ ToDo.md
@@ -0,0 +1,26 @@
+# TO-DO del Proyecto: [ pagGame ] 
+
+## 🎯 En Proceso:
+> _Aquí va la tarea que estás haciendo AHORA MISMO._
+- [ ] Diseñar botón de descarga
+
+## 📝 Tareas para Semana:
+> _Las metas claras que quiero cumplir esta semana. Las elijo del Backlog el lunes._
+- [ ] Añadir footer de contacto
+- [ ] Elegir la paleta de colores
+- [ ] Mejorar lso textos
+- [ ] Cambiar icono y nombre de html #Body#
+
+## 📥 Backlog / Ideas Futuras:
+> _Aquí va cualquier idea, bug o mejora que se te ocurra._
+- [ ] Añadir lista de notas de cambio
+- [ ] 
+- [ ] 
+- [ ] 
+- [ ]
+
+## ✅ Hecho:
+> _¡:D!_
+- [] 
+- [] 
+- [] 
---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+    import { ref } from "vue";
+    const images = ref([
+        "/topos.png",
+        "/catch.png",
+        "/pinpong.png"
+    ]);
+    const current = ref(0);
+
+    setInterval(() => {
+        current.value = (current.value + 1) % images.value.length;
+    }, 5000);
+
+</script>
+
+<template>
+    <h1 class="title">GG RANDOM</h1>
+    <div class="container_body">
+        <div class="carrousel_img">
+            <img :src="images[current]" alt="IMG DE REFERENCIA">
+        </div>
+        
+        <div>
+            <h1>
+                Mini juegos que cambian al azar cuando pierdes.
+            </h1>
+
+            <button>DESCARGAR: Alpha1</button>
+            
+        </div>
+        
+
+    </div>
+    
+</template>
+
+<style>
+    img{
+        width: 50%;
+        position: relative;
+        transform: translateX(50%);
+    }
+
+    .title{
+        color: rgb(255, 255, 255);
+        width: 100%;
+        text-align: center;
+        font-size: clamp(4rem, 2.5vw, 2rem);
+        text-shadow: 4px 5px 5px grey;
+
+        margin-bottom: 10%;
+
+    }
+    .container_body{
+        justify-content: center;
+        display: flex;
+        gap: 1%;
+    }
+
+    h1, h2{
+        color: rgb(255, 255, 255);
+        text-align: center;
+        font-size: clamp(1rem, 22.5vw, 2rem);
+    }
+    button{
+        display: block;
+        margin: 10% auto;
+
+        font-size: clamp(1rem, 22.5vw, 2rem);
+    }
+</style>
